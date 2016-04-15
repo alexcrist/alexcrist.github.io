31dbedbc18254af7445d4bc9f9756709f93fc7ec
Crist, Alex <acrist@mitre.org>
2016-04-15 11:55:06 -0400

    Add night/day button
    - Added night/day button that toggles color scheme
    - Increased color saturation
    - Minor HTML refactoring

---
+++ js/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var FADE_TIME = 1500; // milliseconds
+var BUTTON_TEXT = ['NIGHT', 'DAY'];
+var COLORS = {
+    text: ['black', 'white'],
+    body: ['white', '#333']
+};
+
+var index = 0;
+
+// Toggle light mode on load as a demo
+toggleLightMode();
+
+// Toggle light mode when Night/Day button pressed
+$('#light-button').click(toggleLightMode);
+
+// Toggle light mode
+function toggleLightMode() {
+
+    // Increment color index
+    index = (index + 1) % 2;
+
+    // Toggle text color
+    changeColor('a, .name, .light-button, .content-description', {
+        color: COLORS.text[index],
+        borderColor: COLORS.text[index]
+    });
+
+    // Toggle body color
+    changeColor('body', {
+        backgroundColor: COLORS.body[index]
+    });
+
+    // Toggle text on Night/Day button
+    $('.light-button').text(BUTTON_TEXT[index]);
+}
+
+// Change colors of given elements
+function changeColor(elements, options) {
+    $(elements).animate(options, {
+        duration: FADE_TIME,
+        queue: false
+    }); 
+}
