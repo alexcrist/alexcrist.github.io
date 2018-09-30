6908ce5ec072c01eefde4b2ecd32cde487b97e07
alexcrist <alexecrist@gmail.com>
2018-09-29 20:39:27 -0400

    Overhaul with React/Parcel skeleton

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.scss';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        asdfasdasssdff
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
